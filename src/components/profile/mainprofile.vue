93d829a77040768f20f5eeca476a222ea8114f57
jual ahmed <md.jual.ah@gmail.com>
2018-12-14 22:55:20 +0600

    profile route setting

--- src/components/profile/mainprofile.vue
+++ src/components/profile/mainprofile.vue
@@ -1,87 +1,82 @@
 <template>
 	<div class="row">
 		<div class="col-md-8">
-			
-		
-  	<!-- Card register -->
-   	<div class="card p-5">
 
-     <!-- Card content -->
-     <div class="card-body">
-       <!-- Title -->
-       <h4 class="card-title mb-4">Register here....</h4>
-       <div class="row">
-         <div class="col-12">
-           <div class="form-group">
-             <input type="text" class="form-control form-controlc" placeholder="Email Address">
-           </div>
-           <div class="form-group">
-             <input type="text" class="form-control form-controlc" placeholder="Busniess name">
-           </div>
-           <div class="form-group">
-             <input type="text" class="form-control form-controlc" placeholder="Address">
-           </div>
-            <div class="form-group">
-              <select name="" id="" class="form-control form-controlc">
-                <option value="" selected>Select your country..</option>
-                <option value="">Select your country..</option>
-              </select>
-            </div>
+    	<!-- Card register -->
+     	<div class="card">
+
+        <!-- Card content -->
+        <div class="card-body">
            <div class="row">
-             <div class="col-6">
-              <div class="form-group">
-                <input type="text" class="form-control form-controlc" placeholder="First name">
+              <div class="col-10">
+               <div class="form-group">
+                 <input type="text" class="form-control form-controlc" placeholder="Email Address">
+               </div>
+               <div class="form-group">
+                 <input type="text" class="form-control form-controlc" placeholder="Password">
+               </div>
+               <div class="form-group">
+                 <input type="text" class="form-control form-controlc" placeholder="Compaly name">
+               </div>
+                <div class="form-group">
+                  <select name="" id="" class="form-control form-controlc">
+                    <option value="" selected>Select industry..</option>
+                    <option value="">Select your industry..</option>
+                  </select>
+                </div>
+               <div class="form-group">
+                 <input type="text" class="form-control form-controlc" placeholder="Address">
+               </div>
+                <div class="form-group">
+                  <input type="number" placeholder="Mumber">
+                </div>
+               <div class="row">
+                 <div class="col-6">
+                  <div class="form-group">
+                    <input type="text" class="form-control form-controlc" placeholder="First name">
+                  </div>
+                 </div>
+                 <div class="col-6">
+                  <div class="form-group">
+                    <input type="text" class="form-control form-controlc" placeholder="Last name">
+                  </div>
+                 </div>
+               </div>
+                <div class="form-group">
+                 <input type="text" class="form-control form-controlc" placeholder="Position">
+                </div>
+                <div class="row">
+                 <div class="col-6">
+                  <div class="form-group">
+                    <input type="text" class="form-control form-controlc" placeholder="Phone number">
+                  </div>
+                 </div>
+                 <div class="col-6">
+                  <div class="form-group">
+                    <input type="text" class="form-control form-controlc" placeholder="Industry">
+                  </div>
+                 </div>
+                </div>
+                <div class="form-group">
+                 <input type="text" class="form-control form-controlc" placeholder="Password">
+                </div>
+                <div class="form-group">
+                 <input type="text" class="form-control form-controlc" placeholder="Confirm password">
+                </div>
               </div>
-             </div>
-             <div class="col-6">
-              <div class="form-group">
-                <input type="text" class="form-control form-controlc" placeholder="Last name">
+              <div class="col-2">
+                <div class="form-group pt-1">
+                  <a href="" class="btn btn-blockc" style="font-size: 10px;">Change</a>
+                </div>
+                <div class="form-group mt-4 pt-1">
+                  <a href="" class="btn btn-blockc" style="font-size: 10px;">Change</a>
+                </div>
               </div>
-             </div>
-           </div>
-            <div class="form-group">
-             <input type="text" class="form-control form-controlc" placeholder="Position">
             </div>
-            <div class="row">
-             <div class="col-6">
-              <div class="form-group">
-                <input type="text" class="form-control form-controlc" placeholder="Phone number">
-              </div>
-             </div>
-             <div class="col-6">
-              <div class="form-group">
-                <input type="text" class="form-control form-controlc" placeholder="Industry">
-              </div>
-             </div>
-            </div>
-            <div class="form-group">
-             <input type="text" class="form-control form-controlc" placeholder="Password">
-            </div>
-            <div class="form-group">
-             <input type="text" class="form-control form-controlc" placeholder="Confirm password">
-            </div>
-          </div>
-       
-          <div class="col-12">
-          <div class="form-group">
-            <input type="checkbox">
-            <label for=""><p class="text-infoc pl-1">Trems & Condition</p></label>
-          </div>
-         </div>
-         <div class="col-12">
-          <div class="form-group">
-            <input type="submit" class="btn btn-blockc" value="Log in">
-          </div>
-         </div>
-         <div class="col-12">
-          <p class="text-center">Do not have account? <a @click="openloginform" href="#" class="text-infoc">Log in</a></p>
-            <p class="text-center">Please read our <a href="" class="text-infoc">Privacy policy</a></p>
-         </div>
-       </div>
-     </div>
+        </div>
 
-   	</div>
-  	<!-- Card register end-->
+     	</div>
+    	<!-- Card register end-->
 
   	</div>
 
