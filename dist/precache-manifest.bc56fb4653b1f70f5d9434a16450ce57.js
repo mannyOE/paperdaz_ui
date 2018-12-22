6f171dedff933ef96eaae142c1260b0470efda17
ahmanemmanuel@gmail.com <ahmanemmanuel@gmail.com>
2018-12-22 08:31:02 +0100

    first build

---
+++ dist/precache-manifest.bc56fb4653b1f70f5d9434a16450ce57.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "312661b17a51c31beb1c8f88d0598011",
+    "url": "/img/pdf_icon.png"
+  },
+  {
+    "revision": "37ed28a7e078b686ef7b867b630146c2",
+    "url": "/img/ipad_video_picture.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6b6cfe1d33232fc0416a",
+    "url": "/js/chunk-vendors.a3ee8f86.js"
+  },
+  {
+    "revision": "b337a2b57b9b20190be330a3f43a3467",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89aa65c9ca6c1bd17ba3533964327dfd",
+    "url": "/img/twitter_icon_green.png"
+  },
+  {
+    "revision": "312661b17a51c31beb1c8f88d0598011",
+    "url": "/pdf_icon.png"
+  },
+  {
+    "revision": "ed7f486ad99c61d4ec4e693540ce084f",
+    "url": "/img/youtube_icon_white.png"
+  },
+  {
+    "revision": "cbf71e0461f90c268ca70ca6773016dd",
+    "url": "/img/tutorial_flow.png"
+  },
+  {
+    "revision": "311b607079606d858b3cdcc61d311b8a",
+    "url": "/img/youtube_icon_green.png"
+  },
+  {
+    "revision": "52fe60d9a0c74dfb390cdcb45f99f7ad",
+    "url": "/img/tree_big.png"
+  },
+  {
+    "revision": "4a2724739bae742791619acde26eef89",
+    "url": "/img/twitter_icon_white.png"
+  },
+  {
+    "revision": "afbb6080c7b310e55d2684e76862c49e",
+    "url": "/img/tree_small_gray.png"
+  },
+  {
+    "revision": "b022404128cb7538725129593510e236",
+    "url": "/img/tree_small.png"
+  },
+  {
+    "revision": "ac07f64309b9cac86c05cd94f25fe1f9",
+    "url": "/img/stamf_list_small.png"
+  },
+  {
+    "revision": "f101d1d5640531a889fb8309266b27ab",
+    "url": "/img/linkedin_icon_green.png"
+  },
+  {
+    "revision": "bbf490f605344c263063989501421eb3",
+    "url": "/img/linkedin_icon_white.png"
+  },
+  {
+    "revision": "8dca4b320899fadb11988c6cdc3c1218",
+    "url": "/img/left-img.png"
+  },
+  {
+    "revision": "b6c57cbef3f6bc24fcf14f7ab7c70e50",
+    "url": "/img/magnifying_glass.png"
+  },
+  {
+    "revision": "ef4e95acd3eea4a5a992fe183583107c",
+    "url": "/img/loup_button.png"
+  },
+  {
+    "revision": "8dca4b320899fadb11988c6cdc3c1218",
+    "url": "/img/login-image1.png"
+  },
+  {
+    "revision": "dcaa51e862f98796c25f",
+    "url": "/js/app.38a84bb0.js"
+  },
+  {
+    "revision": "1e98003368d75653c28923699ba502c2",
+    "url": "/img/paperdaz_icon_white.png"
+  },
+  {
+    "revision": "a0ca9141698360be90a06ae27296ac6d",
+    "url": "/img/play_button.png"
+  },
+  {
+    "revision": "a3382d6826b68fe463e7440e24fb1dc8",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "06d8eadb2db320b264e6466307e71151",
+    "url": "/img/remove_icon.png"
+  },
+  {
+    "revision": "888edef0709ea696d2addc2311bf9752",
+    "url": "/img/seeds_gray.png"
+  },
+  {
+    "revision": "4a1daeb0d9c108508baf131f621577d5",
+    "url": "/img/stamf_list_big.png"
+  },
+  {
+    "revision": "b688b366192b88cb13a11dc651b7e923",
+    "url": "/img/seeds.png"
+  },
+  {
+    "revision": "4ab67e1be57109e7596589dae7b96a54",
+    "url": "/img/done_icon.png"
+  },
+  {
+    "revision": "7ea7631c4d0560d577e9ba8c5c3ce6e2",
+    "url": "/img/ipad_logo_inside.png"
+  },
+  {
+    "revision": "e999f43283a53cdd61b06173bc051055",
+    "url": "/img/checked_icon.png"
+  },
+  {
+    "revision": "5793ad8e0028fe1f90eec02448e59a44",
+    "url": "/img/devices.png"
+  },
+  {
+    "revision": "8da1ffb4c29533fb8ed62a86bdf75c26",
+    "url": "/img/email_arrow.png"
+  },
+  {
+    "revision": "6485d56b3afb80eb932c0eb26248f9da",
+    "url": "/img/drop_file_upload.png"
+  },
+  {
+    "revision": "698db119ac5cfa6fc4bb2c11b8ab0f5c",
+    "url": "/img/ipad.png"
+  },
+  {
+    "revision": "2381e6b9c738e3b91c84d7b56ef3e731",
+    "url": "/img/facebook_icon_white.png"
+  },
+  {
+    "revision": "2d99ec6999767ad8bd74221f77c0b256",
+    "url": "/img/facebook_icon_green.png"
+  },
+  {
+    "revision": "f00b6b4f516886a91e6d798f13e1cb3e",
+    "url": "/img/cross_icon.png"
+  },
+  {
+    "revision": "ff16c1826a351f37fd8eb4fedd45ddb2",
+    "url": "/img/bullet_dot_icon.png"
+  },
+  {
+    "revision": "8a26f55d4fc3f68dfd637fed4f2b044a",
+    "url": "/google.png"
+  },
+  {
+    "revision": "6b6cfe1d33232fc0416a",
+    "url": "/css/chunk-vendors.3ef1e4de.css"
+  },
+  {
+    "revision": "dcaa51e862f98796c25f",
+    "url": "/css/app.b31e0eef.css"
+  }
+];
